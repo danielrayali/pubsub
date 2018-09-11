8f9369206e05a882b3849620e0e1b9d7f1118b89
Daniel Ali <dali@keywcorp.com>
2018-09-11 12:32:27 -0400

    Adding ctor/copy/move/assign buffer tests

--- test/buffer_test.cc
+++ test/buffer_test.cc
@@ -6,3 +6,31 @@ using namespace pubsub;
 TEST(BufferTest, ConstructOk) {
 	ASSERT_NO_THROW(Buffer buffer(1024));
 }
+
+TEST(BufferTest, MoveCopyAssignOk) {
+    Buffer buffer(1024);
+    const char* data = "Hello, World!";
+    memcpy(buffer.Get(), data, strlen(data) + 1);
+
+    Buffer copy(buffer);
+    ASSERT_STREQ(reinterpret_cast<char*>(copy.Get()), data);
+
+    Buffer assigned = buffer;
+    ASSERT_STREQ(reinterpret_cast<char*>(assigned.Get()), data);
+
+    Buffer copy_assigned;
+    copy_assigned = buffer;
+    ASSERT_STREQ(reinterpret_cast<char*>(assigned.Get()), data);
+
+    Buffer moved(std::move(buffer));
+    ASSERT_STREQ(reinterpret_cast<char*>(moved.Get()), data);
+
+    // Set up buffer again since it got moved
+    buffer.Allocate(1024);
+    memcpy(buffer.Get(), data, strlen(data) + 1);
+
+    Buffer move_assigned;
+    move_assigned = std::move(buffer);
+    ASSERT_STREQ(reinterpret_cast<char*>(move_assigned.Get()), data);
+}
+
